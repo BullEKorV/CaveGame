051308f56ac5681f7d2f7bb4c2f82f3203dd87be
BullEKorV <eliasw05@hotmail.com>
2022-07-12 14:57:10 +0200

    InitCommit

---
+++ CaveGame/Program.cs
@@ -0,0 +1,19 @@
+﻿global using System.Numerics;
+global using Raylib_cs;
+
+Raylib.InitWindow(1280, 800, "Cavegame");
+
+Game game = new Game();
+
+// Animation.LoadAnimationsFromDirectories();
+
+while (!Raylib.WindowShouldClose())
+{
+    game.Update();
+
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.WHITE);
+    game.Draw();
+
+    Raylib.EndDrawing();
+}
